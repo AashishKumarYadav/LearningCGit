1dd6fc14af503efdaa1e0dd1d147740ab7c3e8da
Aashish Kumar Yadav <aashishy292@gmail.com>
2022-09-19 20:11:49 +0530

    initial commit

---
+++ Sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a,b,sum;
+    a = 2;
+    b = 3;
+
+    sum = a+b;
+    cout<<"Sum of a & b is "<<sum<<endl;
+    
+    return 0;
+}
